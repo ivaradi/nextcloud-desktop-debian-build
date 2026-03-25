9c19ce3ff3e2af0f3b4d89d9f65b9efcd1933b8a
István Váradi <ivaradi@varadiistvan.hu>
2026-03-25 15:45:48 +0100

    The commit file is pushed to the repository only if the build succeeds

--- check-and-build-latest-commit.sh
+++ check-and-build-latest-commit.sh
@@ -24,23 +24,18 @@ commit=$(git ls-remote "${REPOSITORY}" "refs/heads/${branch}" |
 
 commitfiledir="${scriptdir}/commits"
 commitfile="${commitfiledir}/latest-commit-${branch}"
-if test -f "${commitfile}"; then
-    exists="yes"
-else
-    exists="no"
-fi
 
-if test "${exists}" = "no" -o "${commit}" != "$(cat "${commitfile}")"; then
-    mkdir -p "${commitfiledir}"
-    echo "${commit}" > "${commitfile}"
-    if test "${exists}" = "no"; then
-        git -C "${scriptdir}" add "${commitfile}"
-    fi
-    git -C "${scriptdir}" commit -a -m "Updated latest commit for branch ${branch}"
-    git -C "${scriptdir}" push
-
+if test ! -f "${commitfile}" -o "${commit}" != "$(cat "${commitfile}")"; then
     "${scriptdir}/debian-build.sh" "${WORKSPACE}" \
                                    "${commit}" \
                                    "${branch}" "${BRANCH_TYPE}" \
                                    "${REPOSITORY}"
+
+    mkdir -p "${commitfiledir}"
+    echo "${commit}" > "${commitfile}"
+    git -C "${scriptdir}" add "${commitfile}"
+    git -C "${scriptdir}" commit \
+        --message "Updated latest commit for branch ${branch}" \
+        "${commitfile}"
+    git -C "${scriptdir}" push
 fi
